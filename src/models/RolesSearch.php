5c062c532dd8b3cbc62cbfe1c9f614dff7fcb615
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-01-13 00:42:15 +0300

    add models

---
+++ src/models/RolesSearch.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ZakharovAndrew\user\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use ZakharovAndrew\user\models\Roles;
+
+/**
+ * RolesSearch represents the model behind the search form of `app\models\Roles`.
+ */
+class RolesSearch extends Roles
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['title', 'description', 'created_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Roles::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'created_at' => $this->created_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'description', $this->description]);
+
+        return $dataProvider;
+    }
+}
