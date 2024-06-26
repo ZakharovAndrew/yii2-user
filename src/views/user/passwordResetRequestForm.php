dbdd4d570d7967b296966980885130ffa050a8f5
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-06-26 08:43:19 +0300

    Update passwordResetRequestForm.php

--- src/views/user/passwordResetRequestForm.php
+++ src/views/user/passwordResetRequestForm.php
@@ -7,3 +7,15 @@ use ZakharovAndrew\user\Module;
 $this->title = Module::t('Reset password');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
+<?php $form = ActiveForm::begin([]);?>
+    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput() ?>
+
+    <div class="form-group">
+        <div class="col-lg-12">
+            <?= Html::submitButton('Отправить', ['class' => 'login-button']) ?>
+        </div>
+    </div>
+<?php ActiveForm::end(); ?>
