e10b0276010d15577c9534439ce5ca2c145f625b
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-06-26 09:51:58 +0300

    Update passwordResetRequestForm.php

--- src/views/user/passwordResetRequestForm.php
+++ src/views/user/passwordResetRequestForm.php
@@ -3,19 +3,23 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use ZakharovAndrew\user\Module;
- 
+
+$bootstrapVersion = Yii::$app->getModule('user')->bootstrapVersion;
+$classActiveForm = "\\yii\bootstrap".($bootstrapVersion==3 ? '' : $bootstrapVersion)."\\ActiveForm";
+$classHtml = "\\yii\bootstrap".($bootstrapVersion==3 ? '' : $bootstrapVersion)."\\Html";
+
 $this->title = Module::t('Reset password');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<?php $form = ActiveForm::begin([]);?>
+<?php $form = $classActiveForm::begin([]);?>
     <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
     <?= $form->field($model, 'email')->textInput() ?>
 
     <div class="form-group">
         <div class="col-lg-12">
-            <?= Html::submitButton('Отправить', ['class' => 'login-button']) ?>
+            <?= $classHtml::submitButton('Отправить', ['class' => 'login-button']) ?>
         </div>
     </div>
-<?php ActiveForm::end(); ?>
+<?php $classActiveForm::end(); ?>
