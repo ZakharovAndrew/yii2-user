adc35673cd3ef521ac0c1783c4c897b1b138804f
Zakharov Andrey <prostonado_07@mail.ru>
2024-07-15 00:38:11 +0300

    init

---
+++ migrations/m240112_231511_create_user_config_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `user_settings_config`.
+ */
+class m240112_231411_create_user_settings_config_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            'user_settings_config',
+            [
+                'id' => $this->primaryKey(),
+                'title' => $this->string(255)->notNull(),
+                'code' => $this->string(255)->notNull(),
+                'type' => $this->integer(),
+                'access_level' => $this->integer(),
+                'values' => $this->text()
+            ]
+        );
+        
+        // creates index for column `user_id`
+        $this->createIndex(
+            'idx-user-settings-config-code',
+            'user_settings_config',
+            'code'
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('user_settings_config');
+    }
+}
---
+++ src/models/UserSettings.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ZakharovAndrew\user\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user_settings".
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property int|null $setting_config_id
+ * @property string|null $values
+ *
+ * @property UserSettingsConfig $settingConfig
+ */
+class UserSettings extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user_settings';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'setting_config_id'], 'integer'],
+            [['values'], 'string', 'max' => 500],
+            [['setting_config_id'], 'exist', 'skipOnError' => true, 'targetClass' => UserSettingsConfig::class, 'targetAttribute' => ['setting_config_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'setting_config_id' => 'Setting Config ID',
+            'values' => 'Values',
+        ];
+    }
+
+    /**
+     * Gets query for [[SettingConfig]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSettingConfig()
+    {
+        return $this->hasOne(UserSettingsConfig::class, ['id' => 'setting_config_id']);
+    }
+}
