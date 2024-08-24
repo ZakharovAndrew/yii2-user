a16cdd5438fa3da2bc4720767d707e85c1fc7eb5
Zakharov Andrey <prostonado_07@mail.ru>
2024-08-25 01:07:15 +0300

    added birthday widget

---
+++ src/components/BirthdayGreetingWidget.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ZakharovAndrew\user\components;
+
+use Yii;
+use yii\base\Widget;
+
+class BirthdayGreetingWidget extends Widget
+{
+    /**
+     * @var string Custom greeting message
+     */
+    public $message = "Happy Birthday, {username}!";
+
+    public function run()
+    {
+        // Getting the current user
+        $currentUser = Yii::$app->user->identity;
+
+        if ($currentUser) {
+            // Get the current date in month-day format
+            $today = date('m-d');
+
+            // Checking if the user's birthday coincides with today's date
+            if (date('m-d', strtotime($currentUser->birthday)) === $today) {
+                // Replace {username} in the message with the username
+                return str_replace('{username}', $currentUser->username, $this->message);
+            }
+        }
+
+        // If the user's birthday is not celebrated today
+        return '';
+    }
+}
