a53d08f0440b5918ff4cbd2284b43baf0836edf1
Zakharov Andrey <prostonado_07@mail.ru>
2024-08-30 12:36:43 +0300

    added assets

---
+++ src/assets/UserAssets.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link https://github.com/ZakharovAndrew/yii2-user
+ * @copyright Copyright (c) 2024 Zakharov Andrey
+ */
+
+namespace ZakharovAndrew\user\assets;
+
+use yii\web\AssetBundle;
+
+class UserAssets extends AssetBundle
+{
+    public $sourcePath = '@user/assets';
+
+    public $css = [
+        'css/style.css',
+    ];
+
+    public $js = [
+    //    'js/script.js',
+    ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+        //'yii\bootstrap5\BootstrapAsset',
+    ];
+}
