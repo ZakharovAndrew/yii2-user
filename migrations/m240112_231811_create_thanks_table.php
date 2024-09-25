4e305b1293f185a70f4a48407282b7386e573fab
Zakharov Andrey <prostonado_07@mail.ru>
2024-09-26 02:38:03 +0300

    added thanks

--- migrations/m240112_231811_create_thanks_table.php
+++ migrations/m240112_231811_create_thanks_table.php
@@ -3,17 +3,43 @@
 use yii\db\Migration;
 
 /**
- * Initializing roles
+ * Handles the creation of table `thanks`.
  */
-class m240112_231711_add_column_telegam_code_to_user_table extends Migration
+class m240112_231811_create_thanks_table extends Migration
 {
     public function up()
     {
-        $this->addColumn('users', 'telegram_code', $this->string(255)->notNull());
+        $this->createTable('thanks', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'author_id' => $this->integer()->Null(),
+            'text' => $this->text(),
+            'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'),
+        ]);
+
+        $this->addForeignKey(
+            'fk-thanks-user_id',
+            'thanks',
+            'user_id',
+            'user',
+            'id',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk-thanks-author_id',
+            'thanks',
+            'author_id',
+            'user',
+            'id',
+            'SET NULL'
+        );
     }
 
     public function down()
     {
-        $this->dropColumn('users', 'telegram_code');
+        $this->dropForeignKey('fk-thanks-user_id', 'thanks');
+        $this->dropForeignKey('fk-thanks-author_id', 'thanks');
+        $this->dropTable('thanks');
     }
 }
