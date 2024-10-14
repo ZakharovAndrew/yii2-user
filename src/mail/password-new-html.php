6fefefdfe09dcb51881a839d50839db742fcfc34
Zakharov Andrey <prostonado_07@mail.ru>
2024-10-15 00:34:11 +0300

    fixing template

--- src/mail/password-new-html.php
+++ src/mail/password-new-html.php
@@ -7,7 +7,7 @@ $reset_link = Yii::$app->urlManager->createAbsoluteUrl(['site/index']);
 ?>
 
 <div class="password-set">
-    <p><?= Module::t('Hello')?>, <?= Html::encode($user->getName()) ?></p>
+    <h2><?= Module::t('Hello')?>, <?= Html::encode($user->getName()) ?></h2>
     <p><?= Module::t('Your data for access to')?> <?= Html::a(Html::encode($reset_link), $reset_link) ?>:</p>
     <p><?= Module::t('Login') ?>: <?= Html::encode($user->username) ?></p>
     <p><?= Module::t('Password')?>: <?= Html::encode($password) ?></p>
