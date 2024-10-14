5f26167ceaae47e4858999d32fa8ba096a047bfd
Zakharov Andrey <prostonado_07@mail.ru>
2024-10-15 00:35:40 +0300

    fixing template

--- src/mail/password-new-html.php
+++ src/mail/password-new-html.php
@@ -3,12 +3,12 @@
 use yii\helpers\Html;
 use ZakharovAndrew\user\Module;
 
-$reset_link = Yii::$app->urlManager->createAbsoluteUrl(['site/index']);
+$site_link = Yii::$app->urlManager->createAbsoluteUrl(['site/index']);
 ?>
 
 <div class="password-set">
     <h2><?= Module::t('Hello')?>, <?= Html::encode($user->getName()) ?></h2>
-    <p><?= Module::t('Your data for access to')?> <?= Html::a(Html::encode($reset_link), $reset_link) ?>:</p>
+    <p><?= Module::t('Your data for access to')?> <?= Html::a(Html::encode($site_link), $site_link) ?>:</p>
     <p><?= Module::t('Login') ?>: <?= Html::encode($user->username) ?></p>
     <p><?= Module::t('Password')?>: <?= Html::encode($password) ?></p>
     <p><?= Module::t('We recommend changing your password') ?></p>
