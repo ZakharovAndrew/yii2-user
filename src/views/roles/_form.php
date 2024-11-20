e62faf970ce7485fdfeb068d74090f3f90fb4e96
Zakharov Andrey <prostonado_07@mail.ru>
2024-11-21 02:18:53 +0300

    fixed the template

--- src/views/roles/_form.php
+++ src/views/roles/_form.php
@@ -4,6 +4,10 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use ZakharovAndrew\user\Module;
 
+use ZakharovAndrew\user\assets\UserAssets;
+
+UserAssets::register($this);
+
 /** @var yii\web\View $this */
 /** @var ZakharovAndrew\user\models\Roles $model */
 /** @var yii\widgets\ActiveForm $form */
@@ -13,16 +17,17 @@ use ZakharovAndrew\user\Module;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
-    
-    <?= $form->field($model, 'code')->textInput() ?>
-    
-    <?= $form->field($model, 'parameters')->textarea(['rows' => 3]) ?>
-    
-    <?= $form->field($model, 'function_to_get_all_subjects')->textInput(['maxlength' => true]) ?>
+    <div class="white-block">
+        <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
-    
+        <?= $form->field($model, 'code')->textInput() ?>
+
+        <?= $form->field($model, 'parameters')->textarea(['rows' => 3]) ?>
+
+        <?= $form->field($model, 'function_to_get_all_subjects')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+    </div>
     <div class="form-group">
         <?= Html::submitButton(Module::t('Save'), ['class' => 'btn btn-success']) ?>
     </div>
