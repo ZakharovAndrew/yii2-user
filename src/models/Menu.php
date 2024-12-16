53ca6de27b9bc4f1bfa57c381c3afbe94de46421
Zakharov Andrey <prostonado_07@mail.ru>
2024-12-16 21:35:48 +0300

    improved the menu configuration

--- src/models/Menu.php
+++ src/models/Menu.php
@@ -4,6 +4,7 @@ namespace ZakharovAndrew\user\models;
 
 use Yii;
 use ZakharovAndrew\user\models\User;
+use ZakharovAndrew\user\models\UserRoles;
 
 /**
  * Menu for navbar
@@ -14,6 +15,9 @@ use ZakharovAndrew\user\models\User;
  */
 class Menu extends \yii\base\Model
 {
+    // 
+    public static $accessList = null;
+    
     /**
      * Get menu for NavBar
      * @return array
@@ -22,14 +26,14 @@ class Menu extends \yii\base\Model
     {
         $controllersAccessList = Yii::$app->getModule('user')->controllersAccessList;
         
-        $list = User::getAccessList(Yii::$app->user->id);
+        static::$accessList = User::getAccessList(Yii::$app->user->id);
        
         // menu items
         $items = [];
         
         foreach ($controllersAccessList as $controller_id => $params) {
-            if (isset($list[$controller_id])) {
-                $items = array_merge($items, static::getMenuItem($controller_id, $params, $list));
+            if (isset(static::$accessList[$controller_id])) {
+                $items = array_merge($items, static::getMenuItem($controller_id, $params));
             }
         }
         
@@ -41,28 +45,61 @@ class Menu extends \yii\base\Model
      * 
      * @param int $controller_id
      * @param array $items
-     * @param array $list
      * @return array
      */
-    public static function getMenuItem($controller_id, $items, $list)
+    public static function getMenuItem($controller_id, $items)
     {
         $menu_items = [];
+        $actions = explode(',', static::$accessList[$controller_id]);
         
+
         // Processing each menu item
         foreach ($items as $link => $item) {
-            if (is_array($item)) {
-                $menu_items[] = ['label' => $link, 'items' => static::getMenuItem($controller_id, $item, $list)];
+            
+            $url = $link;
+            $label = $item;
+            
+            if (is_array($item) && is_int($link) && isset($item['url'])) {
+                // checking for the role required to display the menu item
+                if (isset($item['roles']) && !static::checkRoles($item['roles'])) {
+                    continue;
+                }
+                
+                $url = $item['url'];
+                $label = $item['label'];
+            } else if (is_array($item)) {
+                $menu_items[] = ['label' => $link, 'items' => static::getMenuItem($controller_id, $item)];
                 continue;
             }
 
-            $actions = explode(',', $list[$controller_id]);
             $arr = explode('/', $link);
-            if ($list[$controller_id] == '*' || in_array(end($arr), $actions)) {
-                $menu_items[] = ['label' => $item, 'url' => [$link]];
+            if (static::$accessList[$controller_id] == '*' || in_array(end($arr), $actions)) {
+                $menu_items[] = ['label' => $label, 'url' => [$url]];
             }
             
         }
         
         return $menu_items;
+    }
+    
+    /**
+     * Checking for the role required to display the menu item
+     * @param string|array $roles
+     * @return boolean
+     */
+    public static function checkRoles($roles)
+    {
+        if (is_string($roles)) {
+            $roles = [$roles];
+        }
+        
+        $userRoles = UserRoles::getUserRoles(Yii::$app->user->id);
+        foreach ($userRoles as $role) {
+            if (in_array($role['code'], $roles)) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
