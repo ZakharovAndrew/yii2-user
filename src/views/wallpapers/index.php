177f13b4560ec8d029a213df6cf44affb527cbe4
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2025-05-16 15:10:45 +0300

    select of wallpapers

---
+++ src/views/wallpapers/index.php
@@ -0,0 +1,24 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $wallpapers array */
+
+$this->title = 'Обои';
+?>
+
+<h1><?= Html::encode($this->title) ?></h1>
+
+<div class="wallpaper-selection">
+    <?php if (empty($wallpapers)): ?>
+        <p>Нет доступных обоев.</p>
+    <?php else: ?>
+        <ul>
+            <?php foreach ($wallpapers as $wallpaper): ?>
+                <li>
+                    <img src="<?= Html::encode($wallpaper) ?>" alt="Wallpaper" style="max-width: 100%; height: auto;">
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+</div>
