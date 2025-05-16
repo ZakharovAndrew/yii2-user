adb1d7de6fd039b89885926d75894003363c36d2
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2025-05-16 16:03:15 +0300

    added wallpapers

--- src/controllers/WallpaperController.php
+++ src/controllers/WallpaperController.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use ZakharovAndrew\user\models\User;
+use ZakharovAndrew\user\models\UserSettings;
 
 class WallpaperController extends Controller
 {
@@ -27,5 +28,17 @@ class WallpaperController extends Controller
         return $this->render('index', [
             'wallpapers' => $availableWallpapers,
         ]);
+    }
+
+    public function actionSelect($wallpaperId)
+    {
+        $userId = Yii::$app->user->id;
+        $settingConfigId = 11; // ID настройки для обоев
+
+        // Сохраняем выбранный фон в пользовательских настройках
+        UserSettings::saveValue($userId, $settingConfigId, $wallpaperId);
+
+        // Перенаправляем обратно на страницу выбора обоев или куда-то еще
+        return $this->redirect(['index']);
     }
 }
