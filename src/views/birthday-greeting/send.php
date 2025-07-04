fce181aea204c778a513caa40c853a21f8498820
Zakharov Andrey <prostonado_07@mail.ru>
2025-07-04 23:22:44 +0300

    added emojiPicker

--- src/views/birthday-greeting/send.php
+++ src/views/birthday-greeting/send.php
@@ -6,6 +6,10 @@ use ZakharovAndrew\user\Module;
 
 $this->title = Module::t('Send birthday congratulations');
 $this->params['breadcrumbs'][] = $this->title;
+
+$this->registerJsFile('https://cdn.jsdelivr.net/npm/vanilla-emoji-picker@0.2.4/dist/emojiPicker.min.js', ['depends' => [\yii\web\JqueryAsset::class]]);
+
+$this->registerJs("new EmojiPicker();");
 ?>
 <div class="birthday-greeting-send">
 
@@ -13,7 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'message')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'message')->textarea(['rows' => 6, 'data-emoji-picker'=>"true"]) ?>
 
     <div class="form-group">
         <?= Html::submitButton(Module::t('Send congratulations'), ['class' => 'btn btn-success']) ?>
