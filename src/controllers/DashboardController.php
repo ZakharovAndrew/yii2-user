09268d64c1c04622d162aa7e1601e1a4508ab862
Zakharov Andrey <prostonado_07@mail.ru>
2026-04-06 22:40:47 +0300

    expanded the dashboard filter

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -22,7 +22,7 @@ class DashboardController extends ParentController
      *
      * @return string
      */
-    public function actionIndex($setting = null)
+    public function actionIndex($setting = null, $setting2 = null, $status = [])
     {
         if (!Yii::$app->user->identity->hasRole('admin')) {
             throw new NotFoundHttpException(Module::t('The requested page does not exist.'));
@@ -31,8 +31,12 @@ class DashboardController extends ParentController
         $settings = ArrayHelper::map(UserSettingsConfig::find()->all(), 'code', 'title');
 
         $setting_name = (!$setting || !isset($settings[$setting])) ? [array_key_first($settings)] : $setting;
+        $setting2_name = (!$setting2 || !isset($settings[$setting2])) ? null : $setting2;
         
-        $data = User::find()->alias('u')
+        
+        
+        if (!$setting2_name) {
+            $data = User::find()->alias('u')
                     ->select(['cnt' => 'count(*)', new \yii\db\Expression("
                         case 
                         when us.values is null or us.values = '' 
@@ -45,14 +49,61 @@ class DashboardController extends ParentController
                     ->leftJoin(UserSettings::tableName(). ' us',
                         "us.setting_config_id = s.id AND us.user_id = u.id"  
                     )
+                    ->where(count($status) ==0 ? [] : ['u.status' => $status])
                     ->groupBy(["setting_value"])
                     ->orderBy("cnt DESC")
                     ->asArray()->all();
+        } else {
+            $data = User::find()->alias('u')
+                    ->select(['cnt' => 'count(*)', new \yii\db\Expression("
+                        case 
+                        when us.values is null or us.values = '' 
+                        then 'Value not set' 
+                        else us.values 
+                        end as setting_value")
+                        , new \yii\db\Expression("
+                        case 
+                        when us2.values is null or us2.values = '' 
+                        then 'Value not set' 
+                        else us2.values 
+                        end as setting2_value")])
+                    ->leftJoin(UserSettingsConfig::tableName(). ' s',
+                        ['s.code' => $setting_name]
+                    )
+                    ->leftJoin(UserSettings::tableName(). ' us',
+                        "us.setting_config_id = s.id AND us.user_id = u.id"  
+                    )
+                    ->leftJoin(UserSettingsConfig::tableName(). ' s2',
+                        ['s2.code' => $setting2_name]
+                    )
+                    ->leftJoin(UserSettings::tableName(). ' us2',
+                        "us2.setting_config_id = s2.id AND us2.user_id = u.id"  
+                    )
+                    ->where(count($status) ==0 ? [] : ['u.status' => $status])
+                    ->groupBy(["setting_value", "setting2_value"])
+                    ->orderBy("cnt DESC")
+                    ->asArray()->all();
+            
+            $setting_column = [];
+            $setting2_column = [];
+            $arr = [];
+            foreach ($data as $row) {
+                $setting_column[$row['setting_value']] = $row['setting_value'];
+                $setting2_column[$row['setting2_value']] = $row['setting2_value'];
+                $arr[$row['setting_value']][$row['setting2_value']] = $row['cnt'];
+            }
+            
+            $data = $arr;
+        }
 
         return $this->render('index', [
             'data' => $data,
             'setting' => $setting,
-            'settings' => $settings
+            'setting2' => $setting2,
+            'settings' => $settings,
+            'setting_column' => $setting_column ?? [],
+            'setting2_column' => $setting2_column ?? [],
+            'status' => $status
         ]);
     }
 }
--- src/views/dashboard/index.php
+++ src/views/dashboard/index.php
@@ -11,29 +11,83 @@ UserAssets::register($this);
 /** @var ZakharovAndrew\user\models\User $model */
 
 $this->title = 'Dashboard';
-?>
 
-<div class="user-profile">
+ // CSS/JS Select2
+$this->registerCssFile('https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css');
+$this->registerJsFile('https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js', ['depends' => [\yii\web\JqueryAsset::class]]);
+// init Select2
+$this->registerJs(<<<JS
+    $('.select2').select2({
+        placeholder: "",
+        allowClear: true,
+        width: '100%',
+        theme: 'bootstrap'
+    });
+JS
+);
+?>
+<style>
+    .table-bordered td {
+        padding: 2px 8px !important;
+    }
+    .table-bordered tr:hover td {
+        background-color:#e0f7fa;
+    }
+    .user-dashboard .select2-container--default .select2-selection--single,
+    .user-dashboard .select2-container .select2-selection--multiple
+    {
+        background: #f5f8fa;
+        border: none;
+    }
+
+</style>
+
+<div class="user-dashboard">
 
     <?php if (Yii::$app->getModule('user')->showTitle) {?><h1><?= Html::encode($this->title) ?></h1><?php } ?>
 
     <div class="white-block">
         <form method="get">
-            <div class="form-group" style="display:flex">
+            <div class="form-group" style="display:flex; gap:10px">
                 <?= Html::dropDownList('setting', $setting, $settings, ['class' => 'form-control form-select'])?>
+                <?= Html::dropDownList('setting2', $setting2, $settings, ['class' => 'form-control form-select', 'prompt' => ''])?>
+                <?= Html::dropDownList('status', $status ?? null, Yii::$app->user->identity::getStatusList(), ['class' => 'form-control form-select select2 ', 'prompt' => '', 'multiple' => true])?>
                 <input type="submit" value="<?= Module::t('Show') ?>" class="btn btn-success" style="margin-left: 10px">
             </div>
         </form>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><?= Module::t('Value') ?></th>
-                    <th><?= Module::t('Number of users') ?></th>
-                </tr>
-            </thead>
-            <?php foreach ($data as $row) {?>
-            <tr><td><?= $row['setting_value'] == 'Value not set' ? Module::t('Value not set') :  $row['setting_value'] ?></td><td><?= $row['cnt'] ?></td></tr>
+        <div class="table-responsive">
+            <?php if (!$setting2) { ?>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><?= Module::t('Value') ?></th>
+                        <th><?= Module::t('Number of users') ?></th>
+                    </tr>
+                </thead>
+                <?php foreach ($data as $row) {?>
+                <tr><td><?= $row['setting_value'] == 'Value not set' ? Module::t('Value not set') :  $row['setting_value'] ?></td><td><?= $row['cnt'] ?></td></tr>
+                <?php } ?>
+            </table>
+            <?php } else { ?>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><?= Module::t('Value') ?></th>
+                        <?php foreach ($setting2_column as $column2) {?>
+                        <th><?= $column2 ?></th>
+                        <?php }?>
+                    </tr>
+                </thead>
+                <?php foreach ($setting_column as $column) {?>
+                    <tr>
+                        <td><?= $column ?></td>
+                        <?php foreach ($setting2_column as $column2) {?>
+                        <td><?= $data[$column][$column2] ?? '' ?></td>
+                        <?php } ?>
+                    </tr>
+                <?php } ?>
+            </table>
             <?php } ?>
-        </table>
+        </div>
     </div>
 </div>
