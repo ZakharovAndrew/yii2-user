1601ffa2b2d8c63e5be0058db4a165b07893ff8b
Zakharov Andrey <prostonado_07@mail.ru>
2026-04-11 23:07:05 +0300

    refactor

--- src/models/BirthdayGreeting.php
+++ src/models/BirthdayGreeting.php
@@ -6,13 +6,37 @@ use Yii;
 use yii\db\ActiveRecord;
 use ZakharovAndrew\user\Module;
 
+/**
+ * BirthdayGreeting model
+ * 
+ * Manages birthday greetings sent between users
+ * 
+ * @property int $id Unique greeting identifier
+ * @property int $user_id ID of the user receiving the greeting
+ * @property int $author_id ID of the user sending the greeting
+ * @property string $message Greeting text message
+ * @property string $created_at Timestamp when greeting was created
+ * @property int $is_read Status flag (0 - unread, 1 - read)
+ * 
+ * @property-read User $user User who receives the greeting
+ * @property-read User $author User who sent the greeting
+ * 
+ * @author Andrew Zakharov
+ * @since 1.0
+ */
 class BirthdayGreeting extends ActiveRecord
 {
+    /**
+     * {@inheritdoc}
+     */
     public static function tableName()
     {
         return 'birthday_greeting';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function rules()
     {
         return [
@@ -25,6 +49,9 @@ class BirthdayGreeting extends ActiveRecord
         ];
     }
     
+    /**
+     * {@inheritdoc}
+     */
     public function attributeLabels()
     {
         return [
@@ -36,34 +63,53 @@ class BirthdayGreeting extends ActiveRecord
         ];
     }
 
+    /**
+     * Gets the user who receives the greeting
+     * 
+     * @return \yii\db\ActiveQuery
+     */
     public function getUser()
     {
         return $this->hasOne(User::className(), ['id' => 'user_id']);
     }
 
+    /**
+     * Gets the user who sent the greeting
+     * 
+     * @return \yii\db\ActiveQuery
+     */
     public function getAuthor()
     {
         return $this->hasOne(User::className(), ['id' => 'author_id']);
     }
     
+    /**
+     * Get formatted creation date with human-readable relative dates
+     * 
+     * Returns date in format "Today 14:30:00", "Yesterday 10:15:00", or "25.12.2024 09:00:00"
+     * 
+     * @return string Formatted date string
+     */
     public function getCreatedAt()
     {
-        $date = date('d.m.Y', strtotime($this->created_at));
-        $time = date('H:i:s', strtotime($this->created_at));
-        $now = date('d.m.Y');
-        $yesterday = date('d.m.Y', strtotime('yesterday'));
-        
-        if ($date == $now) {
-            $date = Module::t('Today');
+        $createdAt = new \DateTime($this->created_at);
+        $now = new \DateTime();
+        $diff = $now->diff($createdAt);
+    
+        if ($diff->days == 0) {
+            return Module::t('Today') . ' ' . $createdAt->format('H:i:s');
+        } elseif ($diff->days == 1) {
+            return Module::t('Yesterday') . ' ' . $createdAt->format('H:i:s');
         }
-        
-        if ($date == $yesterday) {
-            $date = Module::t('Yesterday');
-        }
-        
-        return $date . ' ' . $time;
+    
+        return $createdAt->format('d.m.Y H:i:s');
     }
     
+    /**
+     * Send birthday greeting email to the recipient
+     * 
+     * @return bool Whether the email was sent successfully
+     */
     public function sendEmail()
     {
         /* @var $user User */
